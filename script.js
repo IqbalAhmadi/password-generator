f83a9dd5177aa738eec3a6f7dd1c8c2bb1b7a8a9
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-08-08 12:53:35 -0700

    created function fir Generate Password, craeted loop, & added eventlistener

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-// variable
+// creating variables
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol,
+}
+
+// Event listener for generate password button
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value
+  const hasLower = lowercaseEl.checked
+  const hasUpper = uppercaseEl.checked
+  const hasNumber = numbersEl.checked
+  const hasSymbol = symbolsEl.checked
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  )
+})
+
+// Function to generate password
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = ''
+  const typesCount = lower + upper + number + symbol
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  )
+
+  // Doesn't have a selected type
+  if (typesCount === 0) {
+    return ''
+  }
+
+  // create a loop
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0]
+      generatedPassword += randomFunc[funcName]()
+    })
+  }
+
+  const finalPassword = generatedPassword.slice(0, length)
+
+  return finalPassword
 }
 
 // Functions to get Random letter, numbers, & symbols
