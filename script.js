8ab8a6c9bf81531f09b0466978fee529b65ed027
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-08-08 12:33:39 -0700

    created variable & functions to get random number, letter, & symbols

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// variable
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
 
+// Functions to get Random letter, numbers, & symbols
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97) // this generates a random lower letters
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65) // this generates a random upper letters
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48) // this generates a random number
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)] // this generates a random symbol
+}
